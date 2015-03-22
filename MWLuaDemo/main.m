1fe89356de5b045f016725b51b7395ce80b21b89
morvenyang <morvenyang@morvenyangdeMacBook-Air.local>
2015-03-22 20:14:52 +0800

    Initial Commit

---
+++ MWLuaDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MWLuaDemo
+//
+//  Created by morvenyang on 15/3/22.
+//  Copyright (c) 2015年 morvenyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
